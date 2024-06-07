1bcf89ff19041b125fcc7c0753ac34786c27b0ae
LeticiaAndrade0802 <leticiaandrade0813@gmail.com>
2024-06-07 20:33:13 -0300

    Add files via upload

---
+++ Programa_aula_teste.py
@@ -0,0 +1,27 @@
+import pandas as pd  
+import matplotlib.pyplot as plt
+import streamlit as st
+
+st.header("Dados da empresa 1")
+
+arquivo = "https://raw.githubusercontent.com/LeticiaAndrade0802/aula_teste-/main/empresa1.csv" 
+dfe = pd.read_csv(arquivo, sep=';') 
+st.dataframe(dfe(3)) 
+
+fig, ax = plt.subplots()
+dfe.plot()
+st.puplot(fig)
+
+fig, ax = plt.subplots()
+dfe.plot(kind = 'scatter', x = 'EBITDA', y = 'Lucro operacional')
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+dfe["Lucro do período"].plot(kind = 'hist')
+st.pyplot(fig)
+
+
+st.write(dfe.groupby('Ano').mean())
+
+
+    
